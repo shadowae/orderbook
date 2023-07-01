7e61d57562a76fcfab0d15125b6667f66521b50d
Satish K <satish.appanasamy@gmail.com>
2023-07-01 22:56:17 +1000

    feat: add auto update of data
    Data will auto update every 5 seconds
    Timestamp will verify when it was last updated

--- src/Orderbook.tsx
+++ src/Orderbook.tsx
@@ -3,14 +3,34 @@ import {useEffect, useState} from "react";
 import BarGraph from "./Bargraph";
 
 const Orderbook = () => {
+    const [updatedTS, setUpdatedTS] = useState('')
     const [orders, setOrders] = useState({asks: [], bids: []})
+
     useEffect(() => {
-        getData().then(result => setOrders(result));
-    },[])
+        // Function to fetch data from API and update state
+        const fetchData = async () => {
+            const currentTimestamp = new Date().toLocaleTimeString();
+            const currentDatestamp = new Date().toLocaleDateString();
+            setUpdatedTS(`${currentDatestamp} ${currentTimestamp}`);
+
+            const result = await getData();
+            setOrders(result);
+        };
+
+        // Fetch data initially
+        fetchData();
+
+        // Fetch data every 5 seconds
+        const interval = setInterval(fetchData, 5000);
+
+        // Clean up the interval when component unmounts
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <div>
             <BarGraph orders={orders} />
+            <footer>Last updated on: {updatedTS}</footer>
         </div>
     )
 }
